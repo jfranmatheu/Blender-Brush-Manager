e40a830022df737dd09fa406c6aa8447fe745427
J. Fran Matheu G <45881831+jfranmatheu@users.noreply.github.com>
2023-08-21 14:03:24 +0200

    fix ts util functions, can take a bad mode

--- brush_manager/utils/tool_settings.py
+++ brush_manager/utils/tool_settings.py
@@ -3,24 +3,27 @@ from bpy.types import Brush as BlBrush, Texture as BlTexture, ToolSettings
 
 
 
-def get_ts(context: bpy.types.Context) -> ToolSettings:
+def get_ts(context: bpy.types.Context) -> ToolSettings | None:
     mode = context.mode
     if mode == 'PAINT_GPENCIL':
         mode = 'GPENCIL_PAINT'
     mode = mode.lower()
 
-    return getattr(context.tool_settings, mode)
+    return getattr(context.tool_settings, mode, None)
 
-def get_ts_brush() -> BlBrush:
-    return get_ts(bpy.context).brush
+def get_ts_brush() -> BlBrush | None:
+    if ts := get_ts(bpy.context):
+        return ts.brush
 
-def get_ts_brush_texture_slot() -> bpy.types.BrushTextureSlot:
+def get_ts_brush_texture_slot() -> bpy.types.BrushTextureSlot | None:
     if bl_brush := get_ts_brush():
         return bl_brush.texture_slot
     return None
 
 def set_ts_brush(context: bpy.types.Context, brush: BlBrush) -> None:
-    get_ts(context).brush = brush
+    if ts := get_ts(context):
+        ts.brush = brush
 
 def set_ts_texture(context: bpy.types.Context, texture: BlTexture) -> None:
-    get_ts(context).brush.texture_slot.texture = texture
+    if ts := get_ts(context):
+        ts.brush.texture_slot.texture = texture
