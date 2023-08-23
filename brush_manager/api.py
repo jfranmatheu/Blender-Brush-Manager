479de9b27766e8ded1d51467073839f46c710dd9
J. Fran Matheu G <45881831+jfranmatheu@users.noreply.github.com>
2023-08-23 18:28:24 +0200

    fix import

--- brush_manager/api.py
+++ brush_manager/api.py
@@ -11,7 +11,7 @@ import brush_manager.ops as bm_ops
 
 
 BM_UI = bm_types.UIProps
-BM_DATA = bm_types.AddonData.get() # Utility like the GLOBALS.
+BM_DATA = bm_types.AddonData.get() # Utility like the 
 
 get_bm_data = bm_types.AddonData.get_data_by_context
 
