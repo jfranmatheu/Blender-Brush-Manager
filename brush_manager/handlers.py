63fbc2462429e7d44d1f3eee97958a5c1770ebe5
J. Fran Matheu G <45881831+jfranmatheu@users.noreply.github.com>
2023-08-24 22:06:56 +0200

    handlers were not working properly from this folder, something deeper going on but atm let's have it working

---
+++ brush_manager/handlers.py
@@ -0,0 +1,42 @@
+import bpy
+from bpy.app import handlers
+
+import atexit
+
+first_time = True
+
+
+@handlers.persistent
+def initialize(*args):
+    print("[brush_manager] load_post")
+
+
+@handlers.persistent
+def on_save_post(*args):
+    print("[brush_manager] save_post")
+    from .data import AddonData
+    AddonData.save_all()
+
+
+@atexit.register
+def on_quit():
+    global first_time
+    if not first_time:
+        return
+    print("[brush_manager] atexit")
+    first_time = False
+
+
+# ----------------------------------------------------------------
+
+
+def register():
+    handlers.load_post.append(initialize)
+    handlers.save_post.append(on_save_post)
+
+
+def unregister():
+    if on_save_post in handlers.save_post:
+        handlers.save_post.remove(on_save_post)
+    if initialize in handlers.load_post:
+        handlers.load_post.remove(initialize)
