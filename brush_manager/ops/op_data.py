c4e245d5026cda395ef77ee8b223293efa27b52d
J. Fran Matheu G <45881831+jfranmatheu@users.noreply.github.com>
2023-09-05 13:38:53 +0200

    clear data op

---
+++ brush_manager/ops/op_data.py
@@ -0,0 +1,12 @@
+from brush_manager.addon_utils import Reg
+from brush_manager.paths import Paths
+
+from shutil import rmtree
+
+
+@Reg.Ops.setup
+class ClearData(Reg.Ops.ACTION):
+    def action(self, *args) -> None:
+        data_path = Paths.DATA
+        if data_path.exists() and data_path.is_dir():
+            rmtree(data_path)
--- brush_manager/prefs.py
+++ brush_manager/prefs.py
@@ -3,6 +3,7 @@ from bpy.props import PointerProperty
 
 from .data import AddonData
 from .ops.op_toggle_prefs_ui import ToggleBrushManagerUI as OPS_TogglePrefsUI
+from .ops.op_data import ClearData
 
 
 class BrushManagerPreferences(AddonPreferences):
@@ -15,3 +16,9 @@ class BrushManagerPreferences(AddonPreferences):
         row = layout.row()
         row.scale_y = 5.0
         OPS_TogglePrefsUI.draw_in_layout(row, text="Manage")
+        
+        layout.separator(factor=2)
+        
+        layout.alert = True
+        ClearData.draw_in_layout(row, text="Clear BrushManager Data")
+        layout.alert = False
